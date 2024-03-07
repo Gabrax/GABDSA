2e5d3ebea080b047e54766432402278130805f7e
Gabrax <gab-oz@wp.pl>
2024-03-07 18:03:50 +0100

    Update CF_HalloumiBoxes.cpp

--- Glproj/CF_HalloumiBoxes.cpp
+++ Glproj/CF_HalloumiBoxes.cpp
@@ -9,19 +9,21 @@ int main()
  std::cin >> t;
 
  while(t--){
-    int n,k;
+    int n,k,c;
     std::cin >> n >> k;
-    std::vector<int> arr(n);
+    std::vector<int> arr;
     for(int i = 0; i < n;i++)
     {
-        std::cin >> arr[i];
+        std::cin >> c;
+        arr.push_back(c);
     }
 
     if(std::is_sorted(arr.begin(),arr.end()) || k > 1)
     {
         std::cout << "YES" << '\n';
     }
-    else{
+    else
+    {
         std::cout << "NO" << '\n'; 
     }
  }
