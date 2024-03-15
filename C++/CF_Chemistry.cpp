4edbea7a25dfd523c176f8e88bdb89570b1943de
Gabrax <gab-oz@wp.pl>
2024-03-15 01:21:01 +0100

    chemistry

---
+++ C++/CF_Chemistry.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+
+
+
+int main()
+{
+    int t;
+    std::cin >> t;
+    while(t--){
+        int n,k;
+        std::cin >> n >> k;
+        std::string s;
+        std::cin >> s;
+        std::unordered_map<char, int> frequencyMap;
+        
+        for(const auto& i: s)
+        {
+            frequencyMap[i]++;
+        }
+        int count = 0;
+        for (const auto& pair : frequencyMap) {
+            if (pair.second % 2 != 0) {
+                count++;
+            }
+        }
+        
+
+        if(count <= (k+1))
+        {
+            puts("YES");
+        }else{
+            puts("NO");
+        }
+
+    
+} 
+
+
+std::cin.get();
+}
