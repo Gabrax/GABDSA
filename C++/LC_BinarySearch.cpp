6d6b52ed44b632e85af0a29f3370798ef8b4ca50
Gabrax <gab-oz@wp.pl>
2024-03-22 01:12:04 +0100

    LC_BinarySearch

---
+++ C++/LC_BinarySearch.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <unordered_set>
+#include <set>
+#include <stack>
+#include <queue>
+#include <cctype>
+
+int search(std::vector<int>& nums, int target) {
+        int lo = 0;
+        int hi = nums.size()-1;
+        int mid = 0;
+
+        while(lo<=hi){
+            mid = (lo + hi)/2;
+            if(nums[mid]==target){
+                return mid;
+            }else if(nums[mid]<target){
+                lo=mid+1;
+            }else{
+                hi=mid-1;
+            }
+        }
+        return -1;
+    }
+
+int main(){
+    std::ios::sync_with_stdio(0);
+    std::cin.tie(0);
+
+   std::vector<int> nums = {5};
+    int x = 5;
+
+   std::cout << search(nums,x) << '\n';
+   
+
+    std::cin.get();
+}
