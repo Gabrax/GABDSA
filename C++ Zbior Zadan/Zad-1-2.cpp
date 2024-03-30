cc9fb60b64ccf1d249472146cda82da535a369db
Gabrax <gab-oz@wp.pl>
2024-03-30 16:34:01 +0100

    zbior zadan

---
+++ C++ Zbior Zadan/Zad-1-2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <unordered_set>
+#include <set>
+#include <stack>
+#include <queue>
+#include <cctype>
+
+int main(){
+
+    int t;
+    std::cin >> t;
+    if(t % 2 != 0){
+        puts("nie");
+    }else{
+        puts("tak");
+    }
+
+    return 0;
+}
