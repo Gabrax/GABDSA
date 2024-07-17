99ff7ed4f3d90dede04bee21d1e2594f263b6bff
Gabrax <gab-oz@wp.pl>
2024-07-17 18:09:20 +0200

    changes, added c#

---
+++ C#/ConsoleApp1/ContainsDuplicate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LC
+{
+    public class ContainsDuplicate
+    {
+        public bool IsDistinct(int[] nums)
+        {
+            Dictionary<int, int> result = new Dictionary<int, int>();
+
+            foreach (int x in nums)
+            {
+                if (result.ContainsKey(x)) result[x]++;
+                else result[x] = 1;
+            }
+
+            foreach (var x in result)
+            {
+                if (x.Value > 1)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
