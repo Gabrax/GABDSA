da15cf3226f256cf712ccb11dd833185b9fad4e9
Gabrax <gab-oz@wp.pl>
2024-07-29 23:50:22 +0200

    new pog

---
+++ C++/CF/CF_Mishka_and_Game.cpp
@@ -0,0 +1,24 @@
+#include <cstdio>
+#include <iostream>
+
+
+ int main(){
+
+  int T;
+  std::cin >> T;
+
+   int Acount = 0;
+   int Bcount = 0;
+   while(T--){
+     int a,b;
+     std::cin >> a >> b;
+     if(a > b) Acount++;
+     else if(b > a) Bcount++;
+   }
+  if(Acount > Bcount){
+     puts("Mishka");
+   }else if(Acount < Bcount){
+     puts("Chris");
+   }else puts("Friendship is magic!^^");
+
+ }
